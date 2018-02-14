0ebd86239b17cc030e84a6a593645bd5d2bb6405
Alastair Rayner <alli0@me.com>
2018-02-14 13:56:59 +0000

    Updated comments

---
+++ _TestBuild/Assets/Scripts/Managers/ActionPointSystem.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionPoint : MonoBehaviour
+{
+    // AP points
+    public int APPoints = 0;
+    public int maxAPpoints = 100;
+
+
+
+    // set AP points to max at start
+    void Start()
+    {
+        APPoints = maxAPpoints;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Increment AP by one 
+    public void IncreaseAP()
+    {
+        if(APPoints < maxAPpoints)
+            APPoints++;
+    }
+    // Overload - Increase by a specific amount
+    public void IncreaseAP(int amount)
+    {
+        if((APPoints += amount) < maxAPpoints && amount > 0)
+            APPoints += amount;
+    }
+
+    // Set the AP to a specific value
+    public void SetAP(int value)
+    {
+        //bounds checking
+        if(value > 0 && value < maxAPpoints)
+            APPoints = value;
+    }
+
+    // Get the AP
+    public int GetAP()
+    {
+        return APPoints;
+    }
+
+    // Reset the AP to max
+    public void ResetAP()
+    {
+        APPoints = maxAPpoints;
+    }
+
+    public bool HasAPPoints()
+    {
+        if (APPoints > 0)
+            return true;
+        else
+            return false;
+    }
+
+
+}
+
